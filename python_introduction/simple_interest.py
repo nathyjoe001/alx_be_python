7d4e7e9e6ef1e093d8be187dfe6e482bc6b51427
“Joseph <akpannath84@gmail.com>
2024-12-10 12:29:10 -0800

    Add simple_interest.py to calculate simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,9 @@
+# Define the variables
+principal = 1000 # principal amount in dollars
+rate = 0.05 # Annual interest rate as decimal
+time = 3    # Time in years
+# Calculate the simple interest
+interest = principal * rate * time
+# print the calculated interest
+print(f"The simple interest is: {interest}")
+
